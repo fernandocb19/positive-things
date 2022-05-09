86eecdbb7d9539e5be36815e3bd163cf8b9e4aba
fernandocb19 <102731194+fernandocb19@users.noreply.github.com>
2022-05-09 14:34:17 +0200

    FEAT: Added today date

--- src/app/positive-things-form/positive-things-form.component.ts
+++ src/app/positive-things-form/positive-things-form.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PositiveThingsFormComponent implements OnInit {
 
+  todayDate = new Date();
+
   constructor() { }
 
   ngOnInit(): void {
