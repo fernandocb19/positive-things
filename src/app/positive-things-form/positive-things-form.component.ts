d3e26d055a96b50d45b9cd88e00b73cb13daec64
fernandocb19 <102731194+fernandocb19@users.noreply.github.com>
2022-05-10 14:47:09 +0200

    FEAT: Add the other two fields for complete de three

--- src/app/positive-things-form/positive-things-form.component.ts
+++ src/app/positive-things-form/positive-things-form.component.ts
@@ -1,24 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, AbstractControl } from '@angular/forms';
+import {
+  FormGroup,
+  FormControl,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-positive-things-form',
   templateUrl: './positive-things-form.component.html',
-  styleUrls: ['./positive-things-form.component.css']
+  styleUrls: ['./positive-things-form.component.css'],
 })
 export class PositiveThingsFormComponent implements OnInit {
-
   todayDate = new Date();
   positiveThingsForm!: FormGroup;
- 
-  constructor() { }
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.positiveThingsForm  = new FormGroup({
-      firstPositiveThing: new FormControl('', [Validators.required])
+    this.positiveThingsForm = new FormGroup({
+      firstPositiveThing: new FormControl('', [Validators.required]),
+      secondPositiveThing: new FormControl('', [Validators.required]),
+      thirdPositiveThing: new FormControl('', [Validators.required]),
     });
   }
 
-  get firstPositiveThing() { return this.positiveThingsForm.get('firstPositiveThing'); }
+  get firstPositiveThing() {
+    return this.positiveThingsForm.get('firstPositiveThing');
+  }
 
+  get secondPositiveThing() {
+    return this.positiveThingsForm.get('secondPositiveThing');
+  }
+
+  get thirdPositiveThing() {
+    return this.positiveThingsForm.get('thirdPositiveThing');
+  }
 }
