5d0466132070d7f0cee7d3b207dbc2c67dd9ac95
fernandocb19 <102731194+fernandocb19@users.noreply.github.com>
2022-05-12 15:46:08 +0200

    FEAT: Get day positives thing detail after select a date from datepicker

--- src/app/positive-things-calendar/positive-things-calendar.component.ts
+++ src/app/positive-things-calendar/positive-things-calendar.component.ts
@@ -1,22 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
+import { PositiveJournalDayService } from '../services/positive-journal-day.service';
+import { PositiveJournalDay } from '../models/positive-journal-day.model';
 
 @Component({
   selector: 'app-positive-things-calendar',
   templateUrl: './positive-things-calendar.component.html',
-  styleUrls: ['./positive-things-calendar.component.css']
+  styleUrls: ['./positive-things-calendar.component.css'],
 })
 export class PositiveThingsCalendarComponent implements OnInit {
-
   selectedDate!: string;
+  positiveJournalDay!: PositiveJournalDay | null;
 
-  constructor() { }
+  constructor(private positiveJournalDayService: PositiveJournalDayService) {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   setSelectedDate(date: Date) {
     this.selectedDate = moment(date).format('YYYY-MM-DD');
+    this.getPositiveThingsOfSelectedDay();
   }
 
+  getPositiveThingsOfSelectedDay = () => {
+    const date = moment(this.selectedDate).format('YYYY-MM-DD');
+    this.positiveJournalDayService.getDay(date).subscribe({
+      next: (positiveJournalDay) =>
+        (this.positiveJournalDay = positiveJournalDay),
+      error: () => (this.positiveJournalDay = null),
+    });
+  };
 }
